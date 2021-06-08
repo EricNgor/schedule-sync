db69a3cb71c39664106e9b5287007c9dfe81eed0
EricNgor <EricNgor@users.noreply.github.com>
2021-06-07 23:10:24 -0700

    removed test.js

--- static/js/test.js
+++
@@ -1,8 +0,0 @@
-// let x = [1,2,3];
-// let y = x.findAll(e=>e>=2);
-// console.log(y);
-
-let x = [[1,2,3],[4,5,6],[7,8,9]];
-// for (a of x) {for (b of a) {console.log(b)}}
-let y = x.map(a=>a.map(b=>++b));
-console.log(y);
